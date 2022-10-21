e1964881aa651895fb925cff0c1da4752d8530d3
Dashboard Technology <accounts@dashboardtechnology.co.uk>
2022-10-21 21:22:47 +0100

    fix error in console

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -2,34 +2,36 @@
 
 document.addEventListener('DOMContentLoaded', function(){
   var canvas_collection = document.getElementsByClassName("JohnHancock-canvas");
-  var parent_form = document.getElementById(canvas_collection[0].id).closest("form");
-  const signaturePads = [];
+  if (document.getElementById(canvas_collection[0]){   
+    var parent_form = document.getElementById(canvas_collection[0].id).closest("form");
+    const signaturePads = [];
 
-  for (let i = 0; i < canvas_collection.length; i++) {
-    var element_id = canvas_collection[i].id;
-    var canvas = document.getElementById(element_id);
-    var hidden_field = document.getElementById(element_id+"-hidden");
-    if (canvas && hidden_field) {
-      var signaturePad = new SignaturePad(canvas);
-      signaturePads[i] = signaturePad
-      function resizeCanvas() {
-        let ratio =  Math.max( 1, 1);
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-        signaturePad.clear();
-      }
-      window.addEventListener("resize", resizeCanvas, true);
-      resizeCanvas();
-    }
-  }
-
-  parent_form.onsubmit = function() {
-    for (let i = 0; i < canvas_collection.length; i++){
+    for (let i = 0; i < canvas_collection.length; i++) {
       var element_id = canvas_collection[i].id;
       var canvas = document.getElementById(element_id);
       var hidden_field = document.getElementById(element_id+"-hidden");
-      hidden_field.value = signaturePads[i].toDataURL();
+      if (canvas && hidden_field) {
+        var signaturePad = new SignaturePad(canvas);
+        signaturePads[i] = signaturePad
+        function resizeCanvas() {
+          let ratio =  Math.max( 1, 1);
+          canvas.width = canvas.offsetWidth * ratio;
+          canvas.height = canvas.offsetHeight * ratio;
+          canvas.getContext("2d").scale(ratio, ratio);
+          signaturePad.clear();
+        }
+        window.addEventListener("resize", resizeCanvas, true);
+        resizeCanvas();
+      }
+    }
+
+    parent_form.onsubmit = function() {
+      for (let i = 0; i < canvas_collection.length; i++){
+        var element_id = canvas_collection[i].id;
+        var canvas = document.getElementById(element_id);
+        var hidden_field = document.getElementById(element_id+"-hidden");
+        hidden_field.value = signaturePads[i].toDataURL();
+      }
     }
   }
 }, false);
