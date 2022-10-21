65aa46a2a65d3f4aca5fcaabad4bb51dc0c4ff68
Dashboard Technology <accounts@dashboardtechnology.co.uk>
2022-10-21 15:35:55 +0100

    Js changes: Make it work with multiple canvases in the form

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -1,32 +1,35 @@
 //= require signature_pad
 
 document.addEventListener('DOMContentLoaded', function(){
-  const canvas = document.getElementById("JohnHancock-canvas");
-  const hidden_field = document.getElementById("JohnHancock-hidden");
+  const canvas_collection = document.getElementsByClassName("JohnHancock-canvas");
+  for (let i = 0; i < canvas_collection.length; i++) {
+    element_id = canvas_collection[i].id;
+    const canvas = document.getElementById(element_id);
+    const hidden_field = document.getElementById(element_id+"-hidden");
+    if (canvas && hidden_field) {
+      const parent_form = canvas.closest("form");
+      const signaturePad = new SignaturePad(canvas);
 
-  if (canvas && hidden_field) {
-    const parent_form = canvas.closest("form");
-    const signaturePad = new SignaturePad(canvas);
+      parent_form.onsubmit = function() {
+        hidden_field.value = signaturePad.toDataURL()
+      }
 
-    parent_form.onsubmit = function() {
-      hidden_field.value = signaturePad.toDataURL()
+      function resizeCanvas() {
+        let ratio =  Math.max( 1, 1);
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+        signaturePad.clear();
+      }
+
+      window.addEventListener("resize", resizeCanvas, true);
+      resizeCanvas();
     }
-
-    function resizeCanvas() {
-      let ratio =  Math.max( 1, 1);
-      canvas.width = canvas.offsetWidth * ratio;
-      canvas.height = canvas.offsetHeight * ratio;
-      canvas.getContext("2d").scale(ratio, ratio);
-      signaturePad.clear();
-    }
-
-    window.addEventListener("resize", resizeCanvas, true);
-    resizeCanvas();
-  }
+  }   
 }, false)
 
-function signatureClear() {
-  let canvas = document.getElementById("JohnHancock-canvas");
+function signatureClear(element_id) {
+  let canvas = document.getElementById(element_id);
   let context = canvas.getContext("2d");
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
