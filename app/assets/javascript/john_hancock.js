9268f435dc585710295ac2604bb12392125f8312
Dashboard Technology <accounts@dashboardtechnology.co.uk>
2022-10-21 18:55:09 +0100

    adjust submit handler

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -2,31 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', function(){
   var canvas_collection = document.getElementsByClassName("JohnHancock-canvas");
-  for (let i = 0; i < canvas_collection.length; i++) {
-    var element_id = canvas_collection[i].id;
-    var canvas = document.getElementById(element_id);
-    var hidden_field = document.getElementById(element_id+"-hidden");
-    if (canvas && hidden_field) {
-      var parent_form = canvas.closest("form");
-      var signaturePad = new SignaturePad(canvas);
+  var parent_form = document.getElementById(canvas_collection[0].id).closest("form");
+  parent_form.onsubmit = function() {
+    for (let i = 0; i < canvas_collection.length; i++) {
+      var element_id = canvas_collection[i].id;
+      var canvas = document.getElementById(element_id);
+      var hidden_field = document.getElementById(element_id+"-hidden");
 
-      parent_form.onsubmit = function() {
+      if (canvas && hidden_field) {
+        var signaturePad = new SignaturePad(canvas);
         hidden_field.value = signaturePad.toDataURL()
-      }
 
-      function resizeCanvas() {
-        let ratio =  Math.max( 1, 1);
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-        signaturePad.clear();
-      }
+        function resizeCanvas() {
+          let ratio =  Math.max( 1, 1);
+          canvas.width = canvas.offsetWidth * ratio;
+          canvas.height = canvas.offsetHeight * ratio;
+          canvas.getContext("2d").scale(ratio, ratio);
+          signaturePad.clear();
+        }
 
-      window.addEventListener("resize", resizeCanvas, true);
-      resizeCanvas();
+        window.addEventListener("resize", resizeCanvas, true);
+        resizeCanvas();
+      }
     }
-  }   
-}, false)
+  }
+}, false);
 
 function signatureClear(element_id) {
   var canvas = document.getElementById(element_id);
