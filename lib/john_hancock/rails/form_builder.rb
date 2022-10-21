ef7a65f6fd241ebd86733cddc5bff03f18bc6c7d
Dashboard Technology <accounts@dashboardtechnology.co.uk>
2022-10-21 15:36:12 +0100

    Make it work with multiple canvases in the form

--- lib/john_hancock/rails/form_builder.rb
+++ lib/john_hancock/rails/form_builder.rb
@@ -3,18 +3,18 @@ module JohnHancock
     module FormBuilder
       include ActionView::Helpers::TagHelper
 
-      def signature_canvas
-        content_tag(:canvas, nil, id: 'JohnHancock-canvas')
+      def signature_canvas(element_id)
+        content_tag(:canvas, nil, class: 'JohnHancock-canvas', id: element_id)
       end
 
-      def hidden_signature_field(attribute)
-        hidden_field(attribute.to_sym, id: 'JohnHancock-hidden')
+      def hidden_signature_field(attribute, element_id)
+        hidden_field(attribute.to_sym, id: element_id+'-hidden')
       end
 
-      def signature_field(attribute)
+      def signature_field(attribute, element_id)
         tags = []
-        tags << signature_canvas
-        tags << hidden_signature_field(attribute)
+        tags << signature_canvas(element_id)
+        tags << hidden_signature_field(attribute, element_id)
         tags.join(' ').html_safe
       end
     end
